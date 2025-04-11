0079d5fe26fe6b5ee1d0381956c0a00e0d95b914
Amaan Shaikh <shaikhamaannn100@gmail.com>
2025-04-12 00:16:53 +0530

    Initial Commit

---
+++ src/ui/Loader.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { useNavigation } from "react-router-dom";
+
+function Loader() {
+  // const navigate = useNavigation();
+  // console.log(navigate);
+  return <div className="loader"></div>;
+}
+
+export default Loader;
