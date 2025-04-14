4768ab85fa25c720459138e34d96df5dad7675e8
Amaan Shaikh <shaikhamaannn100@gmail.com>
2025-04-14 23:24:47 +0530

    Completed ui design

--- src/ui/Loader.jsx
+++ src/ui/Loader.jsx
@@ -4,7 +4,11 @@ import { useNavigation } from "react-router-dom";
 function Loader() {
   // const navigate = useNavigation();
   // console.log(navigate);
-  return <div className="loader"></div>;
+  return (
+    <div className="absolute inset-0 flex items-center justify-center bg-slate-200/20 backdrop-blur-sm">
+      <div className="loader"></div>
+    </div>
+  );
 }
 
 export default Loader;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: "Roboto Mono, monospace",
+    },
+    // colors: {
+    //   pizza: "#123456",
+    // },
+
+    extend: {
+      fontSize: {
+        huge: ["80rem", { lineHeight: "1" }],
+      },
+      height: {
+        screen: "100dvh",
+      },
+    },
   },
   plugins: [],
 };
