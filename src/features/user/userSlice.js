e0630fcaefe6a46338c3df8f4779e8f642f0ce9a
Amaan Shaikh <shaikhamaannn100@gmail.com>
2025-04-18 14:27:52 +0530

    Added geolocation feature

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -55,7 +55,10 @@ const userSlice = createSlice({
         state.status = "idle";
       })
       .addCase(fetchAddress.rejected, (state, action) => {
-        (state.status = "error"), (state.error = action.error.message);
+        state.status = "error";
+        state.error =
+          "There was a problem getting your address. Make sure to fill this field!";
+        // state.error = action.error.message;
       }),
 });
 
